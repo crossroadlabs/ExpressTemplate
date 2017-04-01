b1570020847f0e09752131502d49a723e3cfe7a3
Yehor Popovych <y.popovych@crossroadlabs.xyz>
2017-04-01 23:41:54 +0300

    Fixed template code. Pointed to own Express repository for test

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "template",
     dependencies: [
-        .Package(url: "https://github.com/crossroadlabs/Express.git", majorVersion: 0, minor: 3),
+        .Package(url: "https://github.com/ypopovych/Express.git", majorVersion: 0, minor: 3),
     ]
 )
--- Sources/app/main.swift
+++ Sources/app/main.swift
@@ -10,13 +10,13 @@ let app = express()
 
 app.views.register(StencilViewEngine())
 
-app.get("/assets/:file+", action: StaticAction(path: "public", param:"file"))
+app.get(path: "/assets/:file+", action: StaticAction(path: "public", param:"file"))
 
-app.get("/") { (request:Request<AnyContent>)->Action<AnyContent> in
-    return Action<AnyContent>.render("index", context: ["hello": "Hello,", "swift": "Swift", "express": "Express!"])
+app.get(path: "/") { (request:Request<AnyContent>)->Action<AnyContent> in
+    return Action<AnyContent>.render(view: "index", context: ["hello": "Hello,", "swift": "Swift", "express": "Express!"])
 }
 
-app.listen(9999).onSuccess { server in
+app.listen(port: 9999).onSuccess { server in
     print("Express was successfully launched on port", server.port)
 }
 
