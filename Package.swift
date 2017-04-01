cef90d7d882c8a5cc9108e1be42fc36147cb36a4
Yehor Popovych <y.popovych@crossroadlabs.xyz>
2017-04-01 23:08:38 +0300

    Removed Cartfile and Xcode project Moved sources to own folder

--- Package.swift
+++ Package.swift
@@ -2,7 +2,6 @@ import PackageDescription
 
 let package = Package(
     name: "template",
-    exclude: ["views", "dist", "public"],
     dependencies: [
         .Package(url: "https://github.com/crossroadlabs/Express.git", majorVersion: 0, minor: 3),
     ]
